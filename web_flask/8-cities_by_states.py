ef2e4398b6dd168eb93bdf13e6e64428666ad529
Sufi-to <3thomas225@gmail.com>
2024-05-20 03:28:08 +0400

    city states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Starts a Flask web application. group city by states"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_states():
+    """Display an html page with cities grouped by states."""
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
